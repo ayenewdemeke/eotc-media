d8094a12ee0c7d1ea83f48055e100bba9ec80401
Ayenew Y. Demeke <ayennew@gmail.com>
2026-04-11 17:51:38 -0500

    Prisma related issues fixed

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**': ['./generated/**'],
-    },
-  },
   images: {
     remotePatterns: [
       {
